b00ade5c1903eb3ab50acdde467dff385c068959
chaitupenju <chaitanya.penjuri95@gmail.com>
2020-12-12 16:25:18 +0530

    Initial commit

---
+++ app/src/main/java/com/chaitupenju/fallingwords/GameStartActivity.kt
@@ -0,0 +1,14 @@
+package com.chaitupenju.fallingwords
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_game_start.*
+
+class GameStartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_start)
+
+    }
+}
