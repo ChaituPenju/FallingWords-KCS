1d95c3483b2b7128343fe43c33c66b34b4890625
chaitupenju <chaitanya.penjuri95@gmail.com>
2020-12-12 17:38:18 +0530

    Added game score showing activity
    Designed and added player score data displaying activity and recyclerview for displaying list of previous scores

---
+++ app/src/main/java/com/chaitupenju/fallingwords/GameScoreActivity.kt
@@ -0,0 +1,19 @@
+package com.chaitupenju.fallingwords
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameScoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_score)
+
+
+        setupScoresList()
+    }
+
+    private fun setupScoresList() {
+
+    }
+}
